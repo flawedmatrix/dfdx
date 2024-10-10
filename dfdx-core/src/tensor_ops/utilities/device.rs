ad32612a91af916f007714d7ffe9f5fda3fc3fe2
Flawedmatrix <flawedmatrix@gmail.com>
2024-10-10 13:16:30 -0700

    Revert to cudarc 0.9.11 at the cost of support for AMP<f16>

--- dfdx-core/src/tensor_ops/utilities/device.rs
+++ dfdx-core/src/tensor_ops/utilities/device.rs
@@ -132,8 +132,8 @@ impl Device<f64> for crate::tensor::Cpu {}
 
 #[cfg(all(feature = "cuda", feature = "f16"))]
 impl Device<f16> for crate::tensor::Cuda {}
-#[cfg(all(feature = "cuda", feature = "f16"))]
-impl Device<AMP<f16>> for crate::tensor::Cuda {}
+// #[cfg(all(feature = "cuda", feature = "f16"))]
+// impl Device<AMP<f16>> for crate::tensor::Cuda {}
 #[cfg(feature = "cuda")]
 impl Device<f32> for crate::tensor::Cuda {}
 #[cfg(feature = "cuda")]
